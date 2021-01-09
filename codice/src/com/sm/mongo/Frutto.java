a1cfa79e92c8d62bffdf98265be484582f2f640f
Luca Parimbelli <36960844+lucaparimbelli@users.noreply.github.com>
2021-01-09 19:54:04 +0100

    Parsing JSON di prova
    Ho fatto il parsing di un file JSON di prova ("mela") ricevuto da Atlas

---
+++ codice/src/com/sm/mongo/Frutto.java
@@ -0,0 +1,22 @@
+package com.sm.mongo;
+
+public class Frutto {
+	
+	private String frutto;
+	private String size;
+	private String color;
+	
+	public Frutto(String frutto, String size, String color) {
+		this.frutto=frutto;
+		this.size=size;
+		this.color=color;
+	}
+	
+	@Override
+	public String toString() {
+		return frutto +" " + size + " " + color;
+	}
+	
+	
+
+}
