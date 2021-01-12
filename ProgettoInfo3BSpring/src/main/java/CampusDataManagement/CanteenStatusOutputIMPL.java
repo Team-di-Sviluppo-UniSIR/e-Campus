8a5a8adc6016f57c85fa7d4259b2b08eb2ff75e2
Luca Ghislotti <ghislottiluca@gmail.com>
2021-01-12 23:13:33 +0100

    added generic parameters for methods and added support methos to package

--- ProgettoInfo3BSpring/src/main/java/CampusDataManagement/CanteenStatusOutputIMPL.java
+++ ProgettoInfo3BSpring/src/main/java/CampusDataManagement/CanteenStatusOutputIMPL.java
@@ -1,13 +1,13 @@
 package CampusDataManagement;
 
+import JSONParser.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Iterator;
-
 import javax.management.Query;
-
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.json.JSONArray;
@@ -42,14 +42,14 @@ public class CanteenStatusOutputIMPL implements CanteenStatusOutputIF {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	private static Object getKey(JSONArray array, String key, Object filtro) {
 		Object value = -1;
 		for (int i = 0; i < array.length(); i++) {
 			JSONObject item = array.getJSONObject(i);
 			if (item.keySet().contains(key)) {
 				value = item.get(key);
-				if(value.equals((String)filtro))
+				if (value.equals((String) filtro))
 					break;
 			}
 		}
@@ -77,7 +77,7 @@ public class CanteenStatusOutputIMPL implements CanteenStatusOutputIF {
 		FindIterable<Document> queryRes = collection.find(filterQuery);
 
 		// mi assicuro di ricevere 1 solo risultato (il nome della mensa è univoco)
-		if (countQueryResults(queryRes) != 1)
+		if (JSONParser.countQueryResults(queryRes) != 1)
 			throw new RuntimeException(); // definire nostra eccezione
 		else {
 			JSONObject JMensa1 = new JSONObject(queryRes.first().toJson());
@@ -87,20 +87,6 @@ public class CanteenStatusOutputIMPL implements CanteenStatusOutputIF {
 		}
 
 		return postiDisponibili;
-	}
-
-	private static int countQueryResults(FindIterable<Document> docToCount) {
-		int count = 0;
-		for (Document d : docToCount) {
-			count++;
-		}
-
-		/*
-		 * Introdurre test invece di usare questo metodo
-		 * System.out.println("numero di risultati:"+count);
-		 */
-
-		return count;
 	}
 
 	public int getCanteenCapacity(Mensa mensa, MongoClientURI uri) {
@@ -118,7 +104,7 @@ public class CanteenStatusOutputIMPL implements CanteenStatusOutputIF {
 		FindIterable<Document> queryRes = collection.find(filterQuery);
 
 		// mi assicuro di ricevere 1 solo risultato (il nome della mensa � univoco)
-		if (countQueryResults(queryRes) != 1)
+		if (JSONParser.countQueryResults(queryRes) != 1)
 			throw new RuntimeException(); // definire nostra eccezione
 		else {
 			JSONObject JMensa = new JSONObject(queryRes.first().toJson());
@@ -129,16 +115,15 @@ public class CanteenStatusOutputIMPL implements CanteenStatusOutputIF {
 	}
 
 	@Override
-	public double getDishPrice(Mensa mensa, DettaglioApertura dettaglioApertura, Apertura apertura, Menu menu, 
+	public double getDishPrice(Mensa mensa, DettaglioApertura dettaglioApertura, Apertura apertura, Menu menu,
 			Dish dish, MongoClientURI uri) {
-		
+
 		// -1 = errore
 		Double DishPrice = -1.0;
 
 		MongoClient mongoClient = new MongoClient(uri);
 		MongoDatabase mongoDB = mongoClient.getDatabase("DBCampus");
 		MongoCollection<Document> collection = mongoDB.getCollection("DBCampusCollection");
-		
 
 		// final Bson filterQuery = new Document("nome", mensa.nome);
 		Bson filterQuery = Filters.eq("nome", mensa.getNome());
@@ -146,57 +131,35 @@ public class CanteenStatusOutputIMPL implements CanteenStatusOutputIF {
 		FindIterable<Document> queryRes = collection.find(filterQuery);
 
 		// mi assicuro di ricevere 1 solo risultato (il nome della mensa è univoco)
-		if (countQueryResults(queryRes) != 1)
+		if (JSONParser.countQueryResults(queryRes) != 1)
 			throw new RuntimeException();
 		else {
-			JSONObject objMensa = new JSONObject(queryRes.first().toJson());		
-			
+			JSONObject objMensa = new JSONObject(queryRes.first().toJson());
+
 			JSONArray arrayDettagli = objMensa.getJSONArray("dettaglioApertura");
-			ArrayList<String> filterList = new ArrayList<String>(Arrays.asList("giornoSettimana","Lunedì","tipoPasto","Cena"));
-			JSONObject objDettaglioApertura = filterInto(arrayDettagli, filterList);
-			
+			ArrayList<String> filterList = new ArrayList<String>(Arrays.asList("giornoSettimana",
+					dettaglioApertura.getGiornoSettimana(), "tipoPasto", dettaglioApertura.getTipoPasto()));
+			JSONObject objDettaglioApertura = JSONParser.filterInto(arrayDettagli, filterList);
+
 			JSONArray arrayAperture = objDettaglioApertura.getJSONArray("apertura");
-			filterList = new ArrayList<String>(Arrays.asList("data","04/01/2021"));
-			JSONObject objApertura = filterInto(arrayAperture, filterList);
+			filterList = new ArrayList<String>(Arrays.asList("data", apertura.getData().toString()));
+			JSONObject objApertura = JSONParser.filterInto(arrayAperture, filterList);
 			JSONObject objMenu = objApertura.getJSONObject("menu");
-			
+
 			JSONArray arrayPiatti = objMenu.getJSONArray("Piatti");
-			filterList = new ArrayList<String>(Arrays.asList("nomePiatto","Arrosto"));
-			JSONObject result = filterInto(arrayPiatti, filterList);
-						
-			DishPrice = result.getDouble("prezzo");		
-			
-			System.out.println("Il prezzo per " + result.getString("nomePiatto") + " è: "+ DishPrice + "€");		
-			
-			}
-		
-		return DishPrice;		
-	}
-	
-	public JSONObject filterInto(JSONArray startingArray, ArrayList<String> filteringArray){
-		JSONObject resultObj = null;
-		
-		for(int i=0; i<startingArray.length();i++) {
-			JSONObject subarrayObj = startingArray.getJSONObject(i);
-			switch (filteringArray.size()) {
-				case 2: {
-					if(subarrayObj.getString(filteringArray.get(0)).equals(filteringArray.get(1))) {
-						resultObj = subarrayObj;
-						break;
-					}
-				}
-				case 4: {
-					if((subarrayObj.getString(filteringArray.get(0)).equals(filteringArray.get(1)))&&
-					   (subarrayObj.getString(filteringArray.get(2)).equals(filteringArray.get(3)))) {
-						resultObj = subarrayObj;
-						break;
-					}
-				}
-				}
+			filterList = new ArrayList<String>(Arrays.asList("nomePiatto", dish.getNomePiatto()));
+			JSONObject result = JSONParser.filterInto(arrayPiatti, filterList);
+
+			DishPrice = result.getDouble("prezzo");
+
+			System.out.println("Il prezzo per " + result.getString("nomePiatto") + " è: " + DishPrice + "€");
+
 		}
-		
-		return resultObj;
+
+		return DishPrice;
 	}
+
+	
 
 	@Override
 	public Time getCanteenETA() {
---
+++ ProgettoInfo3BSpring/src/main/java/JSONParser/JSONParser.java
@@ -0,0 +1,50 @@
+package JSONParser;
+
+import java.util.ArrayList;
+
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.mongodb.client.FindIterable;
+
+public class JSONParser {
+	public static JSONObject filterInto(JSONArray startingArray, ArrayList<String> filteringArray) {
+		JSONObject resultObj = null;
+
+		for (int i = 0; i < startingArray.length(); i++) {
+			JSONObject subarrayObj = startingArray.getJSONObject(i);
+			switch (filteringArray.size()) {
+			case 2: {
+				if (subarrayObj.getString(filteringArray.get(0)).equals(filteringArray.get(1))) {
+					resultObj = subarrayObj;
+					break;
+				}
+			}
+			case 4: {
+				if ((subarrayObj.getString(filteringArray.get(0)).equals(filteringArray.get(1)))
+						&& (subarrayObj.getString(filteringArray.get(2)).equals(filteringArray.get(3)))) {
+					resultObj = subarrayObj;
+					break;
+				}
+			}
+			}
+		}
+
+		return resultObj;
+	}
+	
+	public static int countQueryResults(FindIterable<Document> docToCount) {
+		int count = 0;
+		for (Document d : docToCount) {
+			count++;
+		}
+
+		/*
+		 * Introdurre test invece di usare questo metodo
+		 * System.out.println("numero di risultati:"+count);
+		 */
+
+		return count;
+	}
+}
