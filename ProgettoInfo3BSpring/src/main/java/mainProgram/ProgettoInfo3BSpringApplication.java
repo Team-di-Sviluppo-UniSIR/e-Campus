6baad19be705834bd8f51d84af66a327f63fcdf8
Alessandro-Mazzola <a.mazzola7@studenti.unibg.it>
2021-01-13 18:30:26 +0100

    Fine fase 2

--- ProgettoInfo3BSpring/src/main/java/mainProgram/ProgettoInfo3BSpringApplication.java
+++ ProgettoInfo3BSpring/src/main/java/mainProgram/ProgettoInfo3BSpringApplication.java
@@ -50,15 +50,15 @@ public class ProgettoInfo3BSpringApplication {
 		// PROVA CAPACITA' MENSA
 		Mensa m1 = new Mensa(0, "I sapori della terra", 0, null);
 		CanteenStatusOutputIF canteenOutput = new CanteenStatusOutputIMPL();
-		// System.out.println("Capacità mensa \"" + m1.getNome() + "\": " +
-		// canteenOutput.getCanteenCapacity(m1, uri));
+		System.out.println("Capacità mensa \"" + m1.getNome() + "\": " +
+				canteenOutput.getCanteenCapacity(m1, uri) + " persone");
 
 		// PROVA POSTI AVAILABLE SEATS APERTURA
 		DettaglioApertura d1 = new DettaglioApertura(0, "Lunedì", "Cena", null, null);
 		Data data1 = new Data("04", "01", "2021");
 		Apertura a1 = new Apertura(0, data1, 0, m1, d1);
-		// System.out.println("Posti disponibili mensa \"" + m1.getNome() + "\": "+
-		// canteenOutput.getAvailableSeats(m1, d1, a1, uri));
+		System.out.println("Posti disponibili mensa \"" + m1.getNome() + "\": "+
+		   canteenOutput.getAvailableSeats(m1, d1, a1, uri));
 
 		// PROVA PREZZO PIATTO
 		Menu menu1 = new Menu(0, "pranzoLunedì", "Mediterraneo", a1);
