1e3e9b4343fc2a67e568dd79db0ee3a2ec5981fe
Luca Ghislotti <ghislottiluca@gmail.com>
2021-01-20 19:27:15 +0100

    aggiornato javadoc

--- JavaDoc/package-search-index.js
+++ JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"CampusDataManagement"},{"l":"DataAggregator"},{"l":"dataItemClasses"},{"l":"dbConnection"},{"l":"JSONParser"},{"l":"mainProgram"},{"l":"Test"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Authentication"},{"l":"CampusDataManagement"},{"l":"CardMgmt"},{"l":"DataAggregator"},{"l":"dataItemClasses"},{"l":"dbConnection"},{"l":"JSONParser"},{"l":"mainProgram"},{"l":"Test"}];updateSearchResults();
